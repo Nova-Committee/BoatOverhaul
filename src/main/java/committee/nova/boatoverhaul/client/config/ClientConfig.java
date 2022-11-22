5b13ff659e12a085fea98e20b87d35785da86881
MikhailTapio <mikhailtapio13468@163.com>
2022-11-22 17:46:55 +0800

    Config

--- src/main/java/committee/nova/boatoverhaul/client/config/ClientConfig.java
+++ src/main/java/committee/nova/boatoverhaul/client/config/ClientConfig.java
@@ -1,4 +1,4 @@
-package committee.nova.boatoverhaul.common.config;
+package committee.nova.boatoverhaul.client.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
@@ -10,7 +10,7 @@ public class ClientConfig {
 
     static {
         final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-        builder.comment("Boat Overhaul Common Config");
+        builder.comment("Boat Overhaul Client Config");
         allowSteeringWhenStopped = builder.comment("Allows a boat to turn its rudder with a very small extra forward speed when the gear state is at STOP")
                 .define("allowRudderingWhenStopped", false);
         reverseRudderWhenSailingAstern = builder.comment("If set to true, a boat sailing backwards and ruddering to right, for example, will sail to the left rearward")
