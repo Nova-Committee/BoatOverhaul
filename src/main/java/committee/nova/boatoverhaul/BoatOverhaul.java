0968242d76bf9a1956f659be7755ad9348f5ba02
MikhailTapio <mikhailtapio13468@163.com>
2022-11-22 19:07:53 +0800

    Clientside friendly

--- src/main/java/committee/nova/boatoverhaul/BoatOverhaul.java
+++ src/main/java/committee/nova/boatoverhaul/BoatOverhaul.java
@@ -1,40 +1,13 @@
 package committee.nova.boatoverhaul;
 
-import committee.nova.boatoverhaul.common.config.Config;
 import committee.nova.boatoverhaul.util.RegistryHandler;
 import net.fabricmc.api.ModInitializer;
 
 public class BoatOverhaul implements ModInitializer {
-    private static boolean allowSteeringWhenStopped;
-    private static boolean reverseRudderWhenSailingAstern;
-    private static Config config;
-
     public static final String MODID = "boatoverhaul";
 
     @Override
     public void onInitialize() {
-        config = Config.of("boatoverhaul-config").provider(path ->
-                "# BoatOverhaul Configs" + "\n" +
-                        "# Allows a boat to turn its rudder with a very small extra forward speed when the gear state is at STOP" + "\n" +
-                        "allowRudderingWhenStopped=false" + "\n" +
-                        "# If set to true, a boat sailing backwards and ruddering to right, for example, will sail to the left rearward" + "\n" +
-                        "reverseRudderWhenSailingAstern=false"
-        ).request();
-        allowSteeringWhenStopped = config.getOrDefault("allowRudderingWhenStopped", false);
-        reverseRudderWhenSailingAstern = config.getOrDefault("reverseRudderWhenSailingAstern", false);
         RegistryHandler.register();
-    }
-
-    @SuppressWarnings("unused")
-    public static Config getConfig() {
-        return config;
-    }
-
-    public static boolean shouldAllowSteeringWhenStopped() {
-        return allowSteeringWhenStopped;
-    }
-
-    public static boolean shouldReverseRudderWhenSailingAstern() {
-        return reverseRudderWhenSailingAstern;
     }
 }
--- src/main/java/committee/nova/boatoverhaul/client/BoatOverhaulClient.java
+++ src/main/java/committee/nova/boatoverhaul/client/BoatOverhaulClient.java
@@ -1,13 +1,49 @@
 package committee.nova.boatoverhaul.client;
 
+import committee.nova.boatoverhaul.common.config.Config;
 import committee.nova.boatoverhaul.common.key.Keys;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 public class BoatOverhaulClient implements ClientModInitializer {
+    private static boolean allowSteeringWhenStopped;
+    private static boolean reverseRudderWhenSailingAstern;
+    private static double speedMultiplier;
+    private static Config config;
+
     @Override
     public void onInitializeClient() {
+        config = Config.of("boatoverhaul-config").provider(path ->
+                "# BoatOverhaul Configs" + "\n" +
+                        "# Allows a boat to turn its rudder with a very small extra forward speed when the gear state is at STOP" + "\n" +
+                        "allowRudderingWhenStopped=false" + "\n" +
+                        "# If set to true, a boat sailing backwards and ruddering to right, for example, will sail to the left rearward" + "\n" +
+                        "reverseRudderWhenSailingAstern=false" + "\n" +
+                        "# The multiplier of the maximum sailing speed" + "\n" +
+                        "speedMultiplier=1.0"
+        ).request();
+        allowSteeringWhenStopped = config.getOrDefault("allowRudderingWhenStopped", false);
+        reverseRudderWhenSailingAstern = config.getOrDefault("reverseRudderWhenSailingAstern", false);
+        speedMultiplier = config.getOrDefault("speedMultiplier", 1.0);
         KeyBindingHelper.registerKeyBinding(Keys.keyLeftRudder);
         KeyBindingHelper.registerKeyBinding(Keys.keyRightRudder);
+    }
+
+
+    @SuppressWarnings("unused")
+    public static Config getConfig() {
+        return config;
+    }
+
+    public static boolean shouldAllowSteeringWhenStopped() {
+        return allowSteeringWhenStopped;
+    }
+
+    public static boolean shouldReverseRudderWhenSailingAstern() {
+        return reverseRudderWhenSailingAstern;
+    }
+
+    public static double getSpeedMultiplier() {
+        return speedMultiplier;
     }
 }
