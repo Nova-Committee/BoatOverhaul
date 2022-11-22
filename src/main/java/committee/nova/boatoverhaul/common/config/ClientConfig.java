e4e8d29ae129685f09c1e894b7bc1d88967314fa
MikhailTapio <mikhailtapio13468@163.com>
2022-11-22 17:40:34 +0800

    Clientside friendly

--- src/main/java/committee/nova/boatoverhaul/common/config/ClientConfig.java
+++ src/main/java/committee/nova/boatoverhaul/common/config/ClientConfig.java
@@ -2,10 +2,11 @@ package committee.nova.boatoverhaul.common.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
-public class CommonConfig {
+public class ClientConfig {
     public static final ForgeConfigSpec CONFIG;
     public static final ForgeConfigSpec.BooleanValue allowSteeringWhenStopped;
     public static final ForgeConfigSpec.BooleanValue reverseRudderWhenSailingAstern;
+    public static final ForgeConfigSpec.DoubleValue speedMultiplier;
 
     static {
         final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
@@ -14,6 +15,8 @@ public class CommonConfig {
                 .define("allowRudderingWhenStopped", false);
         reverseRudderWhenSailingAstern = builder.comment("If set to true, a boat sailing backwards and ruddering to right, for example, will sail to the left rearward")
                 .define("reverseRudderWhenSailingAstern", false);
+        speedMultiplier = builder.comment("The multiplier of the boat's max speed forward.")
+                .defineInRange("speedMultiplier", 1.0, 0.1, 2.0);
         CONFIG = builder.build();
     }
 }
